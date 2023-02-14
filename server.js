0a5b753cab4a4976ae14d27828ce99fc928fe2f9
Gregory Shoup <gregshoup@gmail.com>
2023-02-13 20:11:02 -0500

    gitignore

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
+const dotenv = require('dotenv').config() // allows for environmental variables
+
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+
+
+/////////////////
+// Mongoose
+/////////////////
+
 const mongoose = require('mongoose');
+const Pattern = require('./models/patternSchema.js')
+const e = require('express');
+const { create } = require('./models/patternSchema.js');
 
-const patternSchema = new mongoose.Schema({
-    colors: [String],
-    posterLocation: String,
-    country: String,
-    region: String,
-    city: String
-}, { timestamps: true });
+/////////////////
+// Routes
+/////////////////
 
-const patternModel = mongoose.model('Pattern', patternSchema);
+// route to add new pattern
 
-module.exports = patternModel;
+app.post('/newpattern', (req, res) => {
+    Pattern.create(req.body, (error, createdPattern) => {
+        console.log(createdPattern)
+    })
+})
+
+app.get('/getpatterns', (req, res) => {
+    const requestSize = req.body.size
+    Pattern.aggregate([{ $sample: { size: 1 } }]).exec((error, foundPatterns) => {
+        if (err) {
+            console.log(err)
+        }
+        res.json(foundPatterns)
+    })
+})
