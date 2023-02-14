330db1f25a763569294ff35a0a87ec00c844b682
Gregory Shoup <gregshoup@gmail.com>
2023-02-13 23:42:53 -0500

    ready to test deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 
+const cors = require('cors');
 
 /////////////////
 // Mongoose
@@ -11,8 +12,18 @@ const methodOverride = require('method-override');
 
 const mongoose = require('mongoose');
 const Pattern = require('./models/patternSchema.js')
-const e = require('express');
 const { create } = require('./models/patternSchema.js');
+
+/////////////////
+// Middleware
+/////////////////
+
+// app.use(express.urlencoded({ extended: true }));// standard - this is just how you get url encoded data into JSON
+// const bodyParser = require('body-parser')
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(express.json());
+app.use(cors());
 
 /////////////////
 // Routes
@@ -21,17 +32,46 @@ const { create } = require('./models/patternSchema.js');
 // route to add new pattern
 
 app.post('/newpattern', (req, res) => {
+    // console.log(req.body)
     Pattern.create(req.body, (error, createdPattern) => {
-        console.log(createdPattern)
+        res.json(createdPattern)
     })
 })
 
-app.get('/getpatterns', (req, res) => {
+// app.get('/getpatterns', (req, res) => { // 30 random patterns
+//     const requestSize = req.body.size
+//     Pattern.aggregate([{ $sample: { size: 30 } }]).exec((error, foundPatterns) => {
+//         if (error) {
+//             console.log(error)
+//         }
+//         res.json(foundPatterns)
+//     })
+// })
+
+app.get('/getpatterns', (req, res) => { // 30 random patterns
     const requestSize = req.body.size
-    Pattern.aggregate([{ $sample: { size: 1 } }]).exec((error, foundPatterns) => {
-        if (err) {
-            console.log(err)
+    Pattern.find({}).sort({ updatedAt: -1 }).limit(30).exec((error, foundPatterns) => {
+        if (error) {
+            console.log(error)
         }
         res.json(foundPatterns)
     })
 })
+
+
+// mongoose.connect('mongodb://localhost:27017/microblog', () => { // offline connection for testing while offline
+//     console.log('The connection with mongod local is established');
+// })
+
+mongoose.connect('mongodb+srv://gstar:' + process.env.MONGO_PASSWORD + '@cluster0.lrovc1s.mongodb.net/?retryWrites=true&w=majority', () => {
+    console.log('The connection with mongoDB online is established');
+})
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, () => {
+    console.log("Hello Seattle, I'm listening");
+});
+
